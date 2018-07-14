158167106ddd7d0ea9fb52ae9af11c5fa1ce74a1
Andreas Fuchs <asf@boinkor.net>
2018-07-14 02:11:16 +0200

    Improve formatting

--- init.el
+++ init.el
@@ -1,16 +1,16 @@
-(progn (setq
-        user-init-file (buffer-file-name)
-        user-emacs-directory (file-name-directory user-init-file))
+(progn
+  (setq
+   user-init-file (buffer-file-name)
+   user-emacs-directory "~/Mess/current/straight-repro")
 
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 4))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el")
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-(straight-use-package 'ace-window)
-)
+  (let ((bootstrap-file
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	(bootstrap-version 4))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el")
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+  (straight-use-package 'ace-window))

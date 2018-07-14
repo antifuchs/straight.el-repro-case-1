fa7545cefd7032364cf596b10f79c13e7ac709f6
Andreas Fuchs <asf@boinkor.net>
2018-07-14 02:22:33 +0200

    repro-ify this more

--- init.el
+++ init.el
@@ -13,4 +13,5 @@
 	(goto-char (point-max))
 	(eval-print-last-sexp)))
     (load bootstrap-file nil 'nomessage))
+  (straight-use-package 'epkgs) ; for some reason, if I leave this out, freezing will fail.
   (straight-use-package 'ace-window))
